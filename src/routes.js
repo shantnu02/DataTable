222a4504fd9945e7268803ea912ae7cac1c0370c
SHANTANU SINGH <48873580+shantnu02@users.noreply.github.com>
2021-01-05 22:32:35 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import React from "react";
 const ButtonLoadingSpinner = React.lazy(() =>
   import("pages/ButtonLoadingSpinner"));
-const ReactBasics = React.lazy(() => import("pages/ReactBasics"));
+
 const DataTable = React.lazy(() => import("pages/DataTable"));
 
-const SearchFilter = React.lazy(() => import("pages/SearchFilter"));
 
 
 const routes = [
@@ -24,29 +23,7 @@ const routes = [
     navbar: "Data Table",
     child: null,
   },
- 
-  {
-    enabled: true,
-    path: "/search-filter",
-    component: SearchFilter,
-    navbar: "Search Filter",
-    child: null,
-  },
- 
- 
- 
-  {
-    enabled: true,
-    path: "/react-basics",
-    component: ReactBasics,
-    navbar: "React Basic",
-    child: [
-      {
-        name: "Show and Hide based on State",
-        path: "/react-basics/show-hide-elements",
-      },
-    ],
-  },
+
 ];
 
 export default routes.filter((route) => route.enabled);
