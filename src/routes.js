0abcb6ebd3814ad6496a81c1a53d16700b2a5b92
SHANTANU SINGH <48873580+shantnu02@users.noreply.github.com>
2021-01-05 21:21:50 +0530

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,60 @@
+import React from "react";
+const ButtonLoadingSpinner = React.lazy(() =>
+  import("pages/ButtonLoadingSpinner"));
+const ReactBasics = React.lazy(() => import("pages/ReactBasics"));
+const DataTable = React.lazy(() => import("pages/DataTable"));
+const BuiltWithReact = React.lazy(() => import("pages/BuiltWithReact"));
+const SearchFilter = React.lazy(() => import("pages/SearchFilter"));
+
+
+const routes = [
+  
+  {
+    enabled: true,
+    path: "/button-loader",
+    component: ButtonLoadingSpinner,
+    navbar: "Loading Spinners",
+    child: null,
+  },
+  
+  {
+    enabled: true,
+    path: "/data-table",
+    component: DataTable,
+    navbar: "Data Table",
+    child: null,
+  },
+ 
+ 
+  {
+    enabled: true,
+    path: "/built-with-react",
+    component: BuiltWithReact,
+    navbar: "",
+    child: null,
+  },
+  {
+    enabled: true,
+    path: "/search-filter",
+    component: SearchFilter,
+    navbar: "Search Filter",
+    child: null,
+  },
+ 
+ 
+ 
+  {
+    enabled: true,
+    path: "/react-basics",
+    component: ReactBasics,
+    navbar: "React Basic",
+    child: [
+      {
+        name: "Show and Hide based on State",
+        path: "/react-basics/show-hide-elements",
+      },
+    ],
+  },
+];
+
+export default routes.filter((route) => route.enabled);
