e1287c46b2f2c1d59d0ea9ba6d45293a7ec2f4b2
SHANTANU SINGH <48873580+shantnu02@users.noreply.github.com>
2021-01-05 15:40:07 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-const GoogleMaps = React.lazy(() => import("pages/GoogleMaps"));
 const ButtonLoadingSpinner = React.lazy(() =>
   import("pages/ButtonLoadingSpinner")
 );
@@ -11,30 +10,11 @@ const DataTable = React.lazy(() => import("pages/DataTable"));
 
 const HooksDemo = React.lazy(() => import("pages/HooksDemo"));
 
-const DataTable2 = React.lazy(() => import("pages/DataTable2"));
+
 const BuiltWithReact = React.lazy(() => import("pages/BuiltWithReact"));
 
-// const SearchFilter = React.lazy(() => import("pages/SearchFilter"));
-
-const TreeList = React.lazy(() => import("pages/TreeList"));
-
-const routes = [
-  {
-    enabled: true,
-    path: "/map",
-    component: GoogleMaps,
-    navbar: "google-map-react",
-    child: [
-      {
-        name: "Basic Google Maps",
-        path: "/map",
-      },
-      {
-        name: "Custom Google Maps",
-        path: "/map/custom-style",
-      },
-    ],
-  },
+const routes = 
+  
   {
     enabled: true,
     path: "/button-loader",
@@ -58,13 +38,7 @@ const routes = [
     child: null,
   },
   
-  {
-    enabled: true,
-    path: "/data-table-large",
-    component: DataTable2,
-    navbar: "",
-    child: null,
-  },
+  
  
   {
     enabled: true,
@@ -73,21 +47,8 @@ const routes = [
     navbar: "",
     child: null,
   },
-//   {
-//     enabled: true,
-//     path: "/search-filter",
-//     component: SearchFilter,
-//     navbar: "Search Filter",
-//     child: null,
-//   },
   
-  {
-    enabled: true,
-    path: "/tree-structure",
-    component: TreeList,
-    navbar: "Tree List",
-    child: null,
-  },
+ 
  
   {
     enabled: true,
